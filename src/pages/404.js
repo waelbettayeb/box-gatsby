be8b73072f5d84e886bdd2f406fd5036e57ef33e
WaelMohamedKhelil <khelilwael@gmail.com>
2019-09-21 00:06:44 +0100

    add 404 Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,48 @@
 import React from "react"
 import SEO from "../components/seo"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
+import { Col, Row, Typography } from "antd"
+import { useTranslation } from "react-i18next"
+const { Title, Paragraph, Text } = Typography;
 
-const NotFoundPage = () => (
-  <>
+const NotFoundPage = () => {
+  const {t} = useTranslation('common');
+  const data = useStaticQuery(graphql`
+      query {
+          file(relativePath: { eq: "images/404.png" }) {
+              childImageSharp {
+                  # Specify a fixed image and fragment.
+                  # The default width is 400 pixels
+                  fluid {
+                      ...GatsbyImageSharpFluid
+                  }
+              }
+          }
+      }
+  `)
+  return(<>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
-)
+    <Row type={'flex'} justify={'center'}>
+      <Col xl={12}>
+        <Row type={'flex'} justify={'center'}>
+          <Img fixed={data.file.childImageSharp.fixed} fluid={data.file.childImageSharp.fluid}  style={{width:'100%'}}/>
+        </Row>
+        <Row type={'flex'} justify={'center'}>
+          <Title style={{textAlign:'center'}} level={"3"}>
+            {t('pageNotFound.title')}
+          </Title>
+        </Row>
+        <Row type={'flex'} justify={'center'}>
+          <Paragraph style={{textAlign:'center'}}>
+            {t('pageNotFound.description')}
+          </Paragraph>
+        </Row>
+      </Col>
+    </Row>
+
+  </>);
+}
+
 
 export default NotFoundPage
