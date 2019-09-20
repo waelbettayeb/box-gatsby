05e64468284872b68164c7d51ba463a73bc65f5f
WaelMohamedKhelil <khelilwael@gmail.com>
2019-09-20 22:17:31 +0100

    fix global com icon

--- src/components/Features/index.js
+++ src/components/Features/index.js
@@ -8,32 +8,31 @@ import { useTranslation } from "react-i18next"
 const { Title, Paragraph, Text } = Typography;
 
 export default (props) => {
-    const data = useStaticQuery(graphql`
-        query 
-        {
-            allServicesJson {
-                nodes {
-                    id
-                }
-            }
-        }
-    `)
+  const data = useStaticQuery(graphql`
+      query {
+          allServicesJson {
+              nodes {
+                  id
+              }
+          }
+      }
+  `)
   const {t} = useTranslation('common');
   return (
-      <BottomParallaxSection title={props.title}
-                             paragraph={props.paragraph}
-                             linkButton={{text:<Link to="/services">{t('learnMore')}</Link> }}
-                             background={'white'}
-      >
-          {
-              data.allServicesJson.nodes.map(( node ) => (
-                <Feature
-                  title={t(node.id+'.name')}
-                  iconType={t(node.id+'.iconType')}
-                  description={t(node.id+'.description')}
-                />
-              ))
-          }
-      </BottomParallaxSection>
-    );
+    <BottomParallaxSection title={props.title}
+                           paragraph={props.paragraph}
+                           linkButton={{text:<Link to="/services">{t('learnMore')}</Link> }}
+                           background={'white'}
+    >
+      {
+        data.allServicesJson.nodes.map(( node ) => (
+          <Feature
+            title={t(node.id+'.name')}
+            iconType={t(node.id+'.iconType')}
+            description={t(node.id+'.description')}
+          />
+        ))
+      }
+    </BottomParallaxSection>
+  );
 }
