cb1e6247f7b3fc3b8282fa4aa18865982c95be30
WaelMohamedKhelil <khelilwael@gmail.com>
2019-09-22 00:45:06 +0100

    add offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,18 +27,6 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
         name: `ClassBox Agency`,
         short_name: `ClassBox`,
         start_url: `/`,
@@ -59,6 +47,8 @@ module.exports = {
         }
       },
     },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
